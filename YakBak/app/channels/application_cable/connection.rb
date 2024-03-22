8e63c0a6474a39d93ff13693ca837fa1debf44a6
Wren <OtherwiseJunk@gmail.com>
2024-03-22 15:33:05 -0400

    rename method, add puts

--- YakBak/app/channels/application_cable/connection.rb
+++ YakBak/app/channels/application_cable/connection.rb
@@ -1,4 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # identified by :discord_user
+
+    def connect
+      puts 'in channel connection!'
+      puts params
+    end
   end
 end
--- YakBak/app/channels/chat_channel.rb
+++ YakBak/app/channels/chat_channel.rb
@@ -1,10 +1,11 @@
-class ChatChannel < ApplicationCable::Channel
+class ChatChannel < ApplicationCable::Channel  
   def subscribed
     # stream_from "some_channel"
+    puts 'Got a subscription! that\'s neat.'
     stream_from "chat_#{params[:instance]}"
   end
 
-  def sendMessage(data)
+  def recieve(data)
     puts 'In the receive(data) function'
     puts data
     ActionCable.server.broadcast("chat_#{params[:instance]}", data)
