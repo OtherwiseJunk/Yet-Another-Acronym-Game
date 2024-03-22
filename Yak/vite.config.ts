d05d4f20748617c26968edf9d1ef3b9f109e4434
Wren <OtherwiseJunk@gmail.com>
2024-03-21 23:41:53 -0400

    remove proxy

--- Yak/vite.config.ts
+++ Yak/vite.config.ts
@@ -4,16 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  envDir: '../',
-  server:{
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    },
-  }
+  envDir: '../'
 })
