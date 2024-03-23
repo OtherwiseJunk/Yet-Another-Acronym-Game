f936018bb066681ffd0383dc2cb412d49bef0f8a
Wren <OtherwiseJunk@gmail.com>
2024-03-23 10:26:16 -0400

    replace default logos add title of game

--- Yak/src/App.vue
+++ Yak/src/App.vue
@@ -4,12 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="./assets/yaag_yak.svg" class="logo" alt="YAAG logo" />
+    <h2>Yet Another Acronym Game</h2>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
@@ -17,14 +13,11 @@ import HelloWorld from './components/HelloWorld.vue'
 <style scoped>
 .logo {
   height: 6em;
-  padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
+  margin: 0px;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #ac69ff);
 }
 </style>
