6c8b51464a11cbe43957350a7ed389cb09f23126
Wren <OtherwiseJunk@gmail.com>
2024-03-23 10:25:17 -0400

    fix issue with action cables receiving data.
    add more verbose logging to dev env

--- YakBak/app/channels/chat_channel.rb
+++ YakBak/app/channels/chat_channel.rb
@@ -1,13 +1,10 @@
 class ChatChannel < ApplicationCable::Channel  
   def subscribed
     # stream_from "some_channel"
-    puts 'Got a subscription! that\'s neat.'
     stream_from "chat_#{params[:instance]}"
   end
 
-  def recieve(data)
-    puts 'In the receive(data) function'
-    puts data
+  def receive(data)
     ActionCable.server.broadcast("chat_#{params[:instance]}", data)
   end
 
