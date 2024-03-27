45676a77bd3f1dbd139d2e4faa0c69f06968ba33
Wren <OtherwiseJunk@gmail.com>
2024-03-26 21:24:48 -0400

    split out avatar component. Modify message layout

---
+++ Yak/src/components/Avatar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { watch } from 'vue';
+
+const props = defineProps(['avatarDecorationUrl', 'avatarUrl', 'shouldAnimate'])
+
+watch(props, () =>{
+    setImageSources(props.shouldAnimate);
+});
+let decorationUrl: string;
+let avatarUrl: string;
+setImageSources(props.shouldAnimate);
+
+function setImageSources(shouldAnimate: boolean){
+    decorationUrl = shouldAnimate ? props.avatarDecorationUrl : `${props.avatarDecorationUrl}&passthrough=false`
+    avatarUrl = shouldAnimate ? props.avatarUrl : props.avatarUrl.replace('.gif', '.webp');
+}
+</script>
+
+<template>
+    <div class="avatar-container">
+        <img v-if="avatarDecorationUrl" class="avatar-decoration avatar" :src="decorationUrl">
+    <img class="avatar-icon avatar" :src=" avatarUrl ?? 'https://1219391019515121716.discordsays.com/assets/ninja.png'">
+    </div>
+</template>
+
+<style scoped>
+.avatar-decoration{
+    z-index: 1;
+    height: 48px;
+    left: -4px;
+    top: -24px;
+}
+
+.avatar-icon {
+    height: 40px;
+    border-radius: 50%;
+    z-index: -1;
+    top: -20px;
+    left: 0px;
+}
+
+.avatar {
+    position: absolute;
+    border-radius: 50%;
+}
+
+.avatar-container{
+    position: relative;
+    margin-right: 25px;
+}
+</style>
--- Yak/src/components/MessageComponent.vue
+++ Yak/src/components/MessageComponent.vue
@@ -1,13 +1,10 @@
 <template>
-    <div class="bubbleWrapper">
-        <div class="inlineContainer">
-            <div>
-                <span class="username">{{ author }}</span> <span class="timestamp">{{ new Date(time).toLocaleTimeString() }}</span>
-            </div>
-            <div class="content-and-avatar">
-                <div class="avatar-container">
-                    <img v-if="avatarDecorationUrl" class="avatar-decoration avatar" :src="avatarDecorationUrl">
-                    <img class="avatar-icon avatar" :src=" avatarUrl ?? 'https://1219391019515121716.discordsays.com/assets/ninja.png'">
+    <div class="bubbleWrapper" @mouseenter="hovered" @mouseleave="hovered">
+        <div class="inlineContainer" >
+            <Avatar class="avatar" :avatarDecorationUrl="avatarDecorationUrl" :avatarUrl="avatarUrl" :shouldAnimate="shouldAnimate"></Avatar>
+            <div class="content-and-username">
+                <div>
+                    <span class="username">{{ author }}</span> <span class="timestamp">{{ new Date(time).toLocaleTimeString() }}</span>
                 </div>
                 <div class="bubble">
                     {{ content }}
@@ -18,7 +15,15 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import Avatar from './Avatar.vue'
 const props = defineProps(['content', 'author', 'time', 'avatarUrl', 'avatarDecorationUrl']);
+let shouldAnimate = ref(false)
+
+function hovered(){
+    shouldAnimate.value = !shouldAnimate.value;
+}
+
 console.log(props);
 </script>
 
@@ -36,59 +41,32 @@ console.log(props);
     font-weight: 900;
 }
 
+.avatar{
+    margin-top: 20px;
+}
+
 .bubbleWrapper {
     padding: 10px 10px;
-    display: flex;
-    justify-content: flex-end;
-    flex-direction: column;
-    align-self: flex-end;
     color: #fff;
 }
 
-.content-and-avatar{
+.content-and-username{
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     min-width: 60px;
-    max-width: 700px;
-    align-items: center;
-    justify-items: center;
-}
-
-.avatar-container{
-    position: relative;
-    margin-right: 25px;
-}
-
-.avatar-decoration{
-    z-index: 1;
-    height: 48px;
-    left: -4px;
-    top: -24px;
+    margin-left: 25px;
+    align-items: start;
+    
 }
 
 .inlineContainer {
     display: flex;
-    flex-direction: column;
-    align-items: start;
-}
-
-.avatar-icon {
-    height: 40px;
-    border-radius: 50%;
-    z-index: -1;
-    top: -20px;
-    left: 0px;
-}
-
-.avatar {
-    position: absolute;
-    border-radius: 50%;
+    flex-direction: row;
+    
 }
 
 .bubble {
-    padding: 15px 20px;
-    margin: 6px 8px;
-    border-radius: 16px;
-    width: fit-content
+    width: fit-content;
+    text-align: left;
 }
 </style>
