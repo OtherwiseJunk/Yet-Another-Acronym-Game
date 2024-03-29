6d7b696b25bfd0bceda6ca9468db31e240fe3d1c
Wren <OtherwiseJunk@gmail.com>
2024-03-29 00:26:16 -0400

    track and send message history

--- YakBak/app/channels/chat_channel.rb
+++ YakBak/app/channels/chat_channel.rb
@@ -1,10 +1,25 @@
 class ChatChannel < ApplicationCable::Channel  
+  @@messagesByInstance = {}
+
   def subscribed
+
+    if not messagesByInstance.key?(params[:instance])
+      messagesByInstance[params[:instance]] = Array.new
+    end
+
     # stream_from "some_channel"
     stream_from "chat_#{params[:instance]}"
+
+    # send the current instance history
+    ChatChannel.broadcast_to(
+      discord_user_id,
+      title: 'MessageHistory',
+      body: messagesByInstance[params[:instance]]
+    )
   end
 
   def receive(data)
+    messagesByInstance[params[:instance]] << data
     ActionCable.server.broadcast("chat_#{params[:instance]}", data)
   end
 
