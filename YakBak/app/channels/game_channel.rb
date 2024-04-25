82c9d51612d5d201a22dca34cf78f36610fc413a
Wren <OtherwiseJunk@gmail.com>
2024-04-25 00:22:10 -0400

    bad variable reference

--- YakBak/app/channels/game_channel.rb
+++ YakBak/app/channels/game_channel.rb
@@ -68,7 +68,7 @@ class GameChannel < ApplicationCable::Channel
         end
         return
       end
-      gameState.round_time_remaining -= 1
+      game_state.round_time_remaining -= 1
       sleep(1)
       broadcast_game_state
       broadcast_round_countdown game_state
