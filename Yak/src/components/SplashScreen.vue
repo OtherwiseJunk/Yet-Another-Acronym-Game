c8a0343fddbba883daa58ee276c302f2eb39f6d1
Wren <OtherwiseJunk@gmail.com>
2024-04-25 23:58:42 -0400

    add splash screen

--- Yak/src/components/SplashScreen.vue
+++ Yak/src/components/SplashScreen.vue
@@ -2,7 +2,10 @@
     <Acronym :letterArray="letterArray"></Acronym>
     <br>
     <div class="input">
-        <AnimatedTypingComponent v-if="displayInput" :text="fullText"></AnimatedTypingComponent>
+        <AnimatedTypingComponent v-if="displayInputAutomation" :text="fullText"></AnimatedTypingComponent>
+    </div>
+    <div class="start-button">
+        <button v-if="displayStartButton" @click="clickStart()" class="font">Start</button>
     </div>
 </template>
 
@@ -16,18 +19,44 @@
     background: rgba(255, 255, 255, 0.1);
     backdrop-filter: saturate(150%) blur(10px);
 }
+
+.font {
+    font-size: 2em;
+    font-family: 'Orbitron';
+    font-weight: 800;
+    font-style: normal;
+}
+
+.start-button {
+    height: 100px;
+    margin-top: 40px;
+}
 </style>
 
 <script setup lang="ts">
+const emits = defineEmits(['animation-complete', 'start']);
 import Acronym from './Acronym.vue';
 import AnimatedTypingComponent from './AnimatedTypingComponent.vue'
 import { ref } from 'vue';
 
 const letterArray = ["Y", "A", "A", "G"]
 const fullText = "Yet Another Acronym Game"
-const displayInput = ref(false);
+const displayInputAutomation = ref(false);
+const displayStartButton = ref(false);
 
 setTimeout(() => {
-    displayInput.value = true;
-}, 2500)
+    displayInputAutomation.value = true;
+}, 2300)
+
+setTimeout(() => {
+    emits('animation-complete');
+}, 5500)
+
+setTimeout(() =>{
+    displayStartButton.value = true;
+}, 5700)
+
+function clickStart(){
+    emits('start');
+}
 </script>
