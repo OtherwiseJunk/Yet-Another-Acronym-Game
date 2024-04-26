6cceaa4c7df9ea594b3ad8d5a4f5be2feb72a2cc
Wren <OtherwiseJunk@gmail.com>
2024-04-25 23:57:50 -0400

    add main app strcture

--- Yak/src/App.vue
+++ Yak/src/App.vue
@@ -1,22 +1,38 @@
 <script setup lang="ts">
 // import ChatWindow from './components/ChatWindow.vue';
 // import AnimatedTypingComponent from './components/AnimatedTypingComponent.vue';
+import { ref } from 'vue';
 import SplashScreen from './components/SplashScreen.vue'
+import AnswerSubmission from './components/AnswerSubmission.vue'
+// import { useDiscordStore } from './stores/discordStore';
+import { useGameStore } from './stores/gameStore';
 
 // const title = "Yet Another Acronym Game";
-Array.prototype.shuffle = function(){
-    var i = this.length, j, temp;
-  while(--i > 0){
-    j = Math.floor(Math.random()*(i+1));
-    temp = this[j];
-    this[j] = this[i];
-    this[i] = temp;
-  }
+// const discord = useDiscordStore();
+const cable = useGameStore();
+let phase = ref(0)
+let animationComplete = ref(false);
+let acronym = ref("");
+cable.$subscribe((_, state) =>{
+  phase.value = state.gameState.game_phase
+  acronym.value = state.gameState.current_acronym
+})
+
+function onComplete(){
+  animationComplete.value = true;
 }
+function onStart(){
+  cable.startGame();
+}
+function onSubmit(answer: string){
+  cable.submitAnswer(answer);
+}
+
 </script>
 
 <template>
-  <SplashScreen></SplashScreen>
+  <SplashScreen v-if="phase === 0 || !animationComplete" @animation-complete="onComplete()" @start="onStart()"></SplashScreen>
+  <AnswerSubmission :acronym="acronym" v-if="phase === 1 && animationComplete" @submit="(answer) => onSubmit(answer)"></AnswerSubmission>
 </template>
 
 <style scoped>
