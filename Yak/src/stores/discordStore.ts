d252892b5599b3f6dbc576f9ba73baf9daa5cfb5
Wren <OtherwiseJunk@gmail.com>
2024-04-27 13:53:57 -0400

    refactor and export getUserInformation to take a userId and return a UserData

--- Yak/src/stores/discordStore.ts
+++ Yak/src/stores/discordStore.ts
@@ -1,20 +1,22 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
-import { MessageUserData } from "../models/userData";
+import { UserData } from "../models";
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
 export const useDiscordStore = defineStore("discord", () => {
   let auth = ref<any>(undefined);
-  let currentUserData = ref(new MessageUserData("", "", ""));
+  let currentUserData = ref(new UserData("", "", ""));
   let instanceId = ref("");
   const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+  const defaultImage =
+    "https://cacheblasters.nyc3.cdn.digitaloceanspaces.com/YAAG/yak.png";
 
   async function setup() {
-    await setupDiscordSdk()
+    await setupDiscordSdk();
     console.log("Discord SDK is authenticated");
-      // We can now make API calls within the scopes we requested in setupDiscordSDK()
-      // Note: the access_token returned is a sensitive secret and should be treated as such
-    await setUserInformation();
+    // We can now make API calls within the scopes we requested in setupDiscordSDK()
+    // Note: the access_token returned is a sensitive secret and should be treated as such
+    currentUserData.value = await getUserInformation(auth.value.user.id);
     instanceId.value = discordSdk.instanceId;
   }
 
@@ -38,44 +40,54 @@ export const useDiscordStore = defineStore("discord", () => {
     }
   }
 
-  async function setUserInformation() {
-    let user = auth.value.user;
-    let extension = user.avatar.startsWith("a_") ? "gif" : "webp";
+  async function fetchDiscordResource(resource: string): Promise<any> {
+    let resp = await fetch(resource, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${auth.value.access_token}`,
+      },
+    });
+    return resp.json();
+  }
 
-    fetch(
-      `https://discord.com/api/users/@me/guilds/${discordSdk.guildId}/member`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${auth.value.access_token}`,
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((guildsMembersRead) => {
-        // Retrieve the guild-specific avatar, and fallback to the user's avatar
-        if (guildsMembersRead?.avatar) {
-          currentUserData.value.avatarUrl = `https://cdn.discordapp.com/guilds/${discordSdk.guildId}/users/${user.id}/avatars/${guildsMembersRead.avatar}.${extension}?size=256`;
-        } else if (user.avatar) {
-          currentUserData.value.avatarUrl = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${extension}?size=256`;
-        } else {
-          const defaultAvatarIndex = Math.abs(Number(user.id) >> 22) % 6;
-          currentUserData.value.avatarUrl = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarIndex}.${extension}`;
-        }
+  async function getUserInformation(userId: string): Promise<UserData> {
+    let participants = (
+      await discordSdk.commands.getInstanceConnectedParticipants()
+    ).participants;
+    let user = participants.find((user) => user.id === userId);
+    let userData = new UserData(defaultImage, "", "Unknown User");
 
-        if (guildsMembersRead?.user.avatar_decoration_data) {
-          //https://cdn.discordapp.com/avatar-decoration-presets
-          currentUserData.value.decorationUrl = `https://cdn.discordapp.com/avatar-decoration-presets/${guildsMembersRead.user.avatar_decoration_data.asset}.png?size=256`;
-        }
+    if (user == undefined || user == null) {
+      return userData;
+    }
 
-        // Retrieve the guild-specific nickname, and fallback to the username#discriminator
-        currentUserData.value.displayName =
-          guildsMembersRead?.nick ??
-          user.global_name ??
-          `${user.username}#${user.discriminator}`;
-      });
+    let extension = user.avatar?.startsWith("a_") ? "gif" : "webp";
+
+    let guildUser = await fetchDiscordResource(
+      `https://discord.com/api/users/@me/guilds/${discordSdk.guildId}/member`
+    );
+    // Retrieve the guild-specific avatar, and fallback to the user's avatar
+    if (guildUser?.avatar) {
+      userData.avatarUrl = `https://cdn.discordapp.com/guilds/${discordSdk.guildId}/users/${user.id}/avatars/${guildUser.avatar}.${extension}?size=256`;
+    } else if (user.avatar) {
+      userData.avatarUrl = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${extension}?size=256`;
+    } else {
+      const defaultAvatarIndex = Math.abs(Number(user.id) >> 22) % 6;
+      userData.avatarUrl = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarIndex}.${extension}`;
+    }
+
+    if (guildUser?.user.avatar_decoration_data) {
+      //https://cdn.discordapp.com/avatar-decoration-presets
+      userData.decorationUrl = `https://cdn.discordapp.com/avatar-decoration-presets/${guildUser.user.avatar_decoration_data.asset}.png?size=256`;
+    }
+
+    // Retrieve the guild-specific nickname, and fallback to the username#discriminator
+    userData.displayName =
+      guildUser?.nick ??
+      user.global_name ??
+      `${user.username}#${user.discriminator}`;
+
+    return userData;
   }
 
   async function requestAuthorizationCode() {
@@ -100,5 +112,5 @@ export const useDiscordStore = defineStore("discord", () => {
     });
   }
 
-  return {auth, setup, currentUserData, instanceId}
+  return { auth, setup, currentUserData, instanceId, getUserInformation };
 });
