a063b3065aebc6961fad9e8b8d5fc60ef4a9e541
Wren <OtherwiseJunk@gmail.com>
2024-04-27 13:53:13 -0400

    add reusable waiting for players component

---
+++ Yak/src/components/WaitingForOtherPlayersComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2 class="font">Submitted! Waiting for other players...</h2>
+    <h4 class="smaller-font">{{ submission }}</h4>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps(['submissionText'])
+</script>
+
+<style scoped>
+
+</style>
