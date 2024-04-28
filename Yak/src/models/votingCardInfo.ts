64931bcdf4568875d7c8d23526af33faf2302ad1
Wren <OtherwiseJunk@gmail.com>
2024-04-28 01:13:41 -0400

    add votingCardInfo model

---
+++ Yak/src/models/votingCardInfo.ts
@@ -0,0 +1,10 @@
+export class VotingCardInfo {
+  constructor(
+    public userId: string,
+    public displayName: string,
+    public avatarUrl: string,
+    public decoratorUrl: string,
+    public submissionText: string,
+    public submissionTime: number
+  ) {}
+}
