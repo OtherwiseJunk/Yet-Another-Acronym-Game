dd41fcc637d4aa71581224666cbcc5664511148a
Wren <OtherwiseJunk@gmail.com>
2024-04-28 01:13:01 -0400

    make use of pallette store on splash screen

--- Yak/src/components/SplashScreen.vue
+++ Yak/src/components/SplashScreen.vue
@@ -1,5 +1,5 @@
 <template>
-    <Acronym :letterArray="letterArray"></Acronym>
+    <Acronym :letterArray="letterArray" :colors="colors.acronymPallette"></Acronym>
     <br>
     <div class="input">
         <AnimatedTypingComponent v-if="displayInputAutomation" :text="fullText"></AnimatedTypingComponent>
@@ -38,11 +38,14 @@ const emits = defineEmits(['animation-complete', 'start']);
 import Acronym from './Acronym.vue';
 import AnimatedTypingComponent from './AnimatedTypingComponent.vue'
 import { ref } from 'vue';
+import { usePalletteStore } from '../stores/palletteStore';
 
 const letterArray = ["Y", "A", "A", "G"]
 const fullText = "Yet Another Acronym Game"
 const displayInputAutomation = ref(false);
 const displayStartButton = ref(false);
+const colors = usePalletteStore();
+colors.setAcronymPallette('yaag');
 
 setTimeout(() => {
     displayInputAutomation.value = true;
@@ -52,11 +55,11 @@ setTimeout(() => {
     emits('animation-complete');
 }, 5500)
 
-setTimeout(() =>{
+setTimeout(() => {
     displayStartButton.value = true;
 }, 5700)
 
-function clickStart(){
+function clickStart() {
     emits('start');
 }
 </script>
