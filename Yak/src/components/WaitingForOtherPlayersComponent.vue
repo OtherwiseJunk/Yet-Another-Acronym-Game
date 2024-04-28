83808f5820229aca7419c911594f76826e35ec27
Wren <OtherwiseJunk@gmail.com>
2024-04-28 01:11:53 -0400

    add waiting component for displaying text about waiting

--- Yak/src/components/WaitingForOtherPlayersComponent.vue
+++ Yak/src/components/WaitingForOtherPlayersComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <h2 class="font">Submitted! Waiting for other players...</h2>
-    <h4 class="smaller-font">{{ submission }}</h4>
+    <h4 class="smaller-font">{{ props.submissionText }}</h4>
 </template>
 
 <script setup lang="ts">
@@ -8,5 +8,17 @@
 </script>
 
 <style scoped>
+.font {
+    font-size: 2em;
+    font-family: 'Orbitron';
+    font-weight: 800;
+    font-style: normal;
+}
 
+.smaller-font {
+    font-size: 1.5em;
+    font-family: 'Orbitron';
+    font-weight: 800;
+    font-style: normal;
+}
 </style>
