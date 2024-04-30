8b55ddd61758579bd28dc4d8aeb9490164bd88fc
Wren <OtherwiseJunk@gmail.com>
2024-04-30 01:03:27 -0400

    restore vite config remove yak from docker-compose

--- Yak/vite.config.ts
+++ Yak/vite.config.ts
@@ -4,17 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  envDir: '../',
-  server: {
-    watch: {
-      usePolling: true,
-     },
-     host: true, // Here
-     strictPort: true,
-     port: 80,
-  },
-  preview: {
-    host: true,
-    port: 80
-} 
+  envDir: '../'
 })
