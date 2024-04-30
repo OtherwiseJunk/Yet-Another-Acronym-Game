8a1868fcb88a0e761293120ee26dc138e9ee9a32
Wren <OtherwiseJunk@gmail.com>
2024-04-29 22:52:40 -0400

    include user data in submissions

--- Yak/src/models/userSubmission.ts
+++ Yak/src/models/userSubmission.ts
@@ -1,3 +1,5 @@
+import { UserData } from "./userData";
+
 export class UserSubmission{
-    constructor(public submission: string, public answer_time: number){}
+    constructor(public submission: string, public answer_time: number, public user_data: UserData){}
 }
--- YakBak/app/models/user_submission.rb
+++ YakBak/app/models/user_submission.rb
@@ -1,8 +1,9 @@
 class UserSubmission
   attr_accessor :submission, :answer_time
 
-  def initialize(submission, answer_time)
+  def initialize(submission, answer_time, user_data)
     @submission = submission
     @answer_time = answer_time
+    @user_data = user_data
   end
 end
