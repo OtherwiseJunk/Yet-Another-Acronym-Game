2e032d52ceafcfc80fa148232ba78e8e751a6a38
Wren <OtherwiseJunk@gmail.com>
2024-04-30 13:22:10 -0400

    update vite for github pages

--- Yak/vite.config.ts
+++ Yak/vite.config.ts
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  envDir: '../'
+  envDir: '../',
+  base: '/yet-another-acronym-game'
 })
