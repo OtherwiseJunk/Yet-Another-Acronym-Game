a6c0a85f62ea99275a7d1c2694b6fadb536cc09d
Wren <OtherwiseJunk@gmail.com>
2024-05-08 13:53:41 -0400

    remove db reference

--- YakBak/app/channels/game_channel.rb
+++ YakBak/app/channels/game_channel.rb
@@ -1,5 +1,5 @@
 class GameChannel < ApplicationCable::Channel
-  @redis = Redis.new(host: ENV['REDIS_URL'] || "redis://redis:6379/1", db:'yakbak')
+  @redis = Redis.new(host: ENV['REDIS_URL'] || "redis://redis:6379/1")
   @subscription_count_template = "subscription_count_#{instance_id}}"
   @game_state_template = "game_state_#{instance_id}"
 
