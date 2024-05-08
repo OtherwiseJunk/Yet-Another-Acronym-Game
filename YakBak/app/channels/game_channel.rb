3c6c9c197d3f69d33f69ca69a818c398a38a298b
Wren <OtherwiseJunk@gmail.com>
2024-05-07 21:39:56 -0400

    mutex maybe

--- YakBak/app/channels/game_channel.rb
+++ YakBak/app/channels/game_channel.rb
@@ -1,7 +1,9 @@
 class GameChannel < ApplicationCable::Channel
-  thread_mattr_accessor :game_state_by_instance, :subscription_count_by_instance
-  game_state_by_instance = Hash.new
-  subscription_count_by_instance = Hash.new
+  mutex = Mutex.new
+  mutex.syncronize do
+    @@gameStateByInstance = {}
+    @@subscriptionCountByInstance = {}
+  end
 
   def subscribed
     puts "Subscription request. Instance ID: #{params[:instance]}"
@@ -14,20 +16,23 @@ class GameChannel < ApplicationCable::Channel
   end
 
   def unsubscribed
-    puts "Unsucribe request. Instance ID: #{params[:instance]}"
-    subscription_count_by_instance[params[:instance]] -= 1
-    game_state_by_instance[params[:instance]].remove_player_from_game params[:discordUserId]
+    mutex.syncronize do
+      puts "Unsucribe request. Instance ID: #{params[:instance]}"
+      @@subscriptionCountByInstance[params[:instance]] -= 1
+      @@gameStateByInstance[params[:instance]].remove_player_from_game params[:discordUserId]
 
-    if subscription_count_by_instance[params[:instance]] == 0
-      puts 'Last user has disconnected from instance #{params[:instance]}. Cleaning up...'
+      if @@subscriptionCountByInstance[params[:instance]] == 0
+        puts 'Last user has disconnected from instance #{params[:instance]}. Cleaning up...'
 
-      subscription_count_by_instance = subscription_count_by_instance.delete([params[:instance]]) || Hash.new
-      game_state_by_instance = game_state_by_instance.delete([params[:instance]]) || Hash.new
+        @@subscriptionCountByInstance = @@subscriptionCountByInstance.delete([params[:instance]]) || Hash.new
+        @@gameStateByInstance = @@gameStateByInstance.delete([params[:instance]]) || Hash.new
+      end
     end
   end
 
   def receive(command)
-    game_state = game_state_by_instance[params[:instance]]
+    mutex.syncronize do
+      game_state = @@gameStateByInstance[params[:instance]]
     case command['type']
     when 0
       puts 'received game start request'
@@ -50,34 +55,42 @@ class GameChannel < ApplicationCable::Channel
       puts 'received submission from '
       game_state.handle_player_submission params[:discordUserId], command['data'], params[:instance]
     end
+    end
   end
 
   def increment_subcription_count(instance)
-    if not subscription_count_by_instance.key?(params[:instance])
-      subscription_count_by_instance[params[:instance]] = 1
-    else
-      subscription_count_by_instance[params[:instance]] += 1
+    mutex.syncronize do
+      if not @@subscriptionCountByInstance.key?(params[:instance])
+        @@subscriptionCountByInstance[params[:instance]] = 1
+      else
+        @@subscriptionCountByInstance[params[:instance]] += 1
+      end
     end
   end
 
   def add_player_to_game(player)
-    puts "Got a request to add player #{player} to game. Checking instance dictionary."
-    puts "dictionary: #{game_state_by_instance}"
-    puts "params: #{params}."
-    if not game_state_by_instance.key?(params[:instance])
-      puts("Creating new game")
-      game_state_by_instance[params[:instance]] = GameState.new player
-    else
-      puts("Adding player to existing game")
-      game_state_by_instance[params[:instance]].add_player_to_game player
+    mutex.syncronize do
+      puts "Got a request to add player #{player} to game. Checking instance dictionary."
+      puts "dictionary: #{@@gameStateByInstance}"
+      puts "params: #{params}."
+      if not @@gameStateByInstance.key?(params[:instance])
+        puts("Creating new game")
+        @@gameStateByInstance[params[:instance]] = GameState.new player
+      else
+        puts("Adding player to existing game")
+        @@gameStateByInstance[params[:instance]].add_player_to_game player
+      end
     end
   end
 
   def broadcast_game_state
-    ActionCable.server.broadcast "game_#{params[:instance]}", game_state_by_instance[params[:instance]]
+    mutex.syncronize do
+      ActionCable.server.broadcast "game_#{params[:instance]}", @@gameStateByInstance[params[:instance]]
+    end
   end
 
   def broadcast_round_countdown(game_state)
+    mutex.syncronize do
       while game_state.round_time_remaining > 0
         game_state.round_second_elapsed
         sleep 1
@@ -87,19 +100,19 @@ class GameChannel < ApplicationCable::Channel
           break
         end
 
-        unless game_state_by_instance.key?(params[:instance])
+        unless @@gameStateByInstance.key?(params[:instance])
           puts 'Game has ended, bailing on countdown.'
           break
         end
         broadcast_game_state
       end
 
-      unless game_state_by_instance.key?(params[:instance])
+      unless @@gameStateByInstance.key?(params[:instance])
         puts 'Game has ended, bailing on countdown.'
         return
       end
       game_state.next_phase
       broadcast_game_state
+    end
   end
-
 end
